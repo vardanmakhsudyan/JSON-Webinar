20eea8a2f5ef8f08a9477f137decf2e109f12d2a
Vardan Makhsudyan <sdf>
2020-08-03 00:03:47 +0300

    Initial Commit

---
+++ JSONWebinar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONWebinar
+//
+//  Created by MacBook Air on 03.08.2020.
+//  Copyright © 2020 VardanMakhsudyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
